5b38d742cdcb0168ba8b7428bb31781e538500c4
gharbiakram <akramgharbi13@gmail.com>
2025-04-22 23:01:16 +0100

    added the user management screen & user data

---
+++ front_end/userdata.js
@@ -0,0 +1,5 @@
+export const userList = [
+    { id: "u1", name: "Alice", isBlocked: false },
+    { id: "u2", name: "Bob", isBlocked: true },
+    { id: "u3", name: "Charlie", isBlocked: false }
+];
